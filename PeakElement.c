8fc4b44d3118a7afe9d80896659f0ee7735b74d5
Krishna36519 <69285898+Krishna36519@users.noreply.github.com>
2026-05-10 10:54:21 +0530

    Modified the program

--- PeakElement.c
+++ PeakElement.c
@@ -12,23 +12,11 @@ int main(){
     for(int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
-    // Check first element
-    if(n == 1 || arr[0] > arr[1]){
-        printf("Peak element: %d", arr[0]);
-        return 0;
-    }
-
-    // Check middle elements
-    for(int i = 1; i < n-1; i++){
-        if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+    for(int i = 0; i < n; i++){
+        if((i == 0 || arr[i] > arr[i-1]) && (i == n-1 || arr[i] > arr[i+1])){
             printf("Peak element: %d", arr[i]);
             return 0;
         }
-    }
-
-    // Check last element
-    if(arr[n-1] > arr[n-2]){
-        printf("Peak element: %d", arr[n-1]);
     }
 
     return 0;
